7a5d0cb626ac36d4b7d63543345d66e1b063938a
黄亮 <h.love.pure@gmail.com>
2021-11-10 11:28:26 +0800

    feat: add .d.ts支持

--- .fatherrc.js
+++ .fatherrc.js
@@ -2,26 +2,27 @@ export default {
   esm: {
     type: 'babel',
     mjs: true,
-    minify: true,
+    minify: true
   },
   cjs: {
     type: 'babel',
     lazy: true,
-    minify: true,
+    minify: true
   },
   umd: {
     globals: {
       react: 'React',
+      ramda: 'ramda',
       '@tarojs/taro': 'Taro',
-      querystring: 'querystring',
+      querystring: 'querystring'
     },
     name: 'taro-use-connect-printer',
     file: 'taro-use-connect-printer',
     minFile: true,
-    sourcemap: true,
+    sourcemap: true
   },
   injectCSS: false, // 不注入css
   extractCSS: false, // 单独提取css
   runtimeHelpers: true,
-  disableTypeCheck: true,
+  disableTypeCheck: true
 };
